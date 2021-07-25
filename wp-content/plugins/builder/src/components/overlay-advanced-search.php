b2275ae5659f6d81a8215352c351a293998e5dcb
Gina <gina@ginaluttrell.com>
2021-07-25 19:07:26 +0000

    Advanced search file (again?)

---
+++ wp-content/plugins/builder/src/components/overlay-advanced-search.php
@@ -0,0 +1,14 @@
+<head>
+	<meta charset="UTF-8" />
+	<title>Universal Palette Builder – Firedrake Beauty Labs</title>
+	<link rel="stylesheet" type="text/css" href="../../css/variables.css" />
+	<link rel="stylesheet" type="text/css" href="../../css/basic-styles.css" />
+	<link rel="stylesheet" type="text/css" href="../../css/builder-styles.css" />
+	<link rel="stylesheet" href="https://use.typekit.net/mxy5ujb.css" />
+</head>
+<body>
+	<section class="Drawer_Overlay">
+		<section class="Advanced_Search_Filter"></section>
+		<section class="Advanced_Search_Filter"></section>
+	</section>
+</body>
