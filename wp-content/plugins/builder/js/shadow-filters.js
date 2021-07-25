cc400bbfa15731321bac868507eea62b0c5b58f2
Imani <lathomas64@gmail.com>
2021-07-25 17:58:13 -0400

    update filter js

--- wp-content/plugins/builder/js/shadow-filters.js
+++ wp-content/plugins/builder/js/shadow-filters.js
@@ -1,3 +1,41 @@
-function filter(event) {
-	console.log(event.target);
+var PRICE_MIN = -1;
+var PRICE_MAX = -1;
+var FILTERS = {"colors": new Set(),
+			   "finishes": new Set(),
+			   "characteristics": new Set(),
+				"countries": new Set()}
+
+function Toggle_Filter(event, filterset) {
+	var filter = event.target;
+	var type = filter.id;
+	var state = filter.checked;
+
+	if(state) {
+		FILTERS[filterset].add(type);
+	} else {
+		FILTERS[filterset].delete(type);
+	}
+	update();
+}
+
+function update(){
+	let matched = new Set();
+	let unmatched = new Set();
+	const shadows = document.getElementsByClassName("Single_Pan_Card");
+
+	//filter colors
+	//filter finishes
+	//filter price
+	//filter characteristics
+	//filter countries
+	console.log('update called');
+	console.log('or filters:');
+	console.log(OR_FILTERS);
+	console.log('and filters:');
+	console.log(AND_FILTERS);
+}
+
+
+funcion match_red(element) {
+	
 }
