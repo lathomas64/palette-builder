ca5412d86c35d7ac006367a96ed0a0fe57219607
Gina <gina@ginaluttrell.com>
2021-07-28 00:38:39 +0000

    Maybe I got it right this time?

---
+++ wp-content/plugins/builder/src/filter-sections/brand-characteristics.php
@@ -0,0 +1,15 @@
+<div class="Section_Title">
+	<div class="Heading">Brand Characteristics</div>
+</div>
+<div class="Button_Group">
+	<ul class="Row Gap_8 Justify_Content_Flex_Start">
+		<li>
+			<input onclick='Checkbox_And_Filter(event);' class="Filter_Button" id="CF" type="checkbox" />
+			<label class="Filter_Button" for="CF">Cruelty-Free</label>
+		</li>
+		<li>
+			<input onclick='Checkbox_And_Filter(event);' class="Filter_Button" id="Veg" type="checkbox" />
+			<label class="Filter_Button" for="Veg">Vegan</label>
+		</li>
+	</ul>
+</div>
---
+++ wp-content/plugins/builder/src/filter-sections/finish.php
@@ -0,0 +1,27 @@
+<div class="Section_Title">
+	<div class="Heading">Finish</div>
+</div>
+<div class="Button_Group">
+	<ul class="Row Gap_4">
+		<li>
+			<input onclick='Checkbox_Or_Filter(event);' class="Filter_Button Matte Dark"id="Matte" type="checkbox" />
+			<label class="Filter_Button"for="Matte">Matte</label>
+		</li>
+		<li>
+			<input onclick='Checkbox_Or_Filter(event);'class="Filter_Button" id="Satin" type="checkbox" />
+			<label class="Filter_Button Satin Dark"for="Satin">Satin</label>
+		</li>
+		<li>
+			<input onclick='Checkbox_Or_Filter(event);' class="Filter_Button"id="Shimmer" type="checkbox" />
+			<label class="Filter_Button Shimmer Dark"for="Shimmer">Shimmer</label>
+		</li>
+		<li>
+			<input onclick='Checkbox_Or_Filter(event);' class="Filter_Button"id="Metallic" type="checkbox" />
+			<label class="Filter_Button Metallic Light"for="Metallic">Metallic</label>
+		</li>
+		<li>
+			<input onclick='Checkbox_Or_Filter(event);' class="Filter_Button"id="Sparkle" type="checkbox" />
+			<label class="Filter_Button Sparkle Light"for="Sparkle">Sparkle</label>
+		</li>
+	</ul>
+</div>
