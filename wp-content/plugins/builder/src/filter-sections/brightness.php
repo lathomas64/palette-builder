22a3d48cc244bc0a8380439cabc3ea4a1b134b42
Gina <gina@ginaluttrell.com>
2021-07-28 17:47:17 +0000

    I forgot why I needed to work off the server and now I feel silly

---
+++ wp-content/plugins/builder/src/filter-sections/brightness.php
@@ -0,0 +1,27 @@
+<div class="Section_Title Lightness">
+	<div class="Heading">Lightness</div>
+</div>
+<div class="Button_Group">
+	<ul class="Row Gap_4">
+		<li>
+			<input class="Filter_Button Whitened Dark"id="Whitened" type="checkbox" />
+			<label class="Filter_Button"for="Whitened">Whitened</label>
+		</li>
+		<li>
+			<input class="Filter_Button" id="Light" type="checkbox" />
+			<label class="Filter_Button Light Dark"for="Light">Light</label>
+		</li>
+		<li>
+			<input  class="Filter_Button"id="Mid-Toned" type="checkbox" />
+			<label class="Filter_Button Mid-Toned Dark"for="Mid-Toned">Mid-Toned</label>
+		</li>
+		<li>
+			<input  class="Filter_Button"id="Dark" type="checkbox" />
+			<label class="Filter_Button Dark Light"for="Dark">Dark</label>
+		</li>
+		<li>
+			<input  class="Filter_Button"id="Blackened" type="checkbox" />
+			<label class="Filter_Button Blackened Light"for="Blackened">Blackened</label>
+		</li>
+	</ul>
+</div>
