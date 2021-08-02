d4158c73850b864e29a11dbadd6ae9f0bee75ab4
Gina <gina@ginaluttrell.com>
2021-08-02 02:31:57 +0000

    Making the irregularity correct… I guess

--- wp-content/plugins/builder/src/builder.php
+++ wp-content/plugins/builder/src/builder.php
@@ -105,25 +105,17 @@
 					</div>
 					<div class="Builder Row Gap_0 Justify_Content_Flex_Start Align_Items_Center">
 						<div class="Set_Aside Column Gap_0">
-							<div class="Shadow_Image_Container Column Align_Items_Center Justify_Content_Center Pan_Size_37 Pan_Shape_Round">
+							<div class="Shadow_Image_Container Column Align_Items_Center Justify_Content_Center Pan_Size_26 Pan_Shape_Round">
 								<div class="Wrapper">
-									<img src="../assets/temp-shadow-imgs/Arsenic.jpg" />
+									<img src="../assets/temp-shadow-imgs/Helix-Nebula.jpg" />
 									<div class="Pan_Shadow">
 										<?php include "components/pan-controls.php"; ?>
 									</div>
 								</div>
 							</div>
-							<div class="Shadow_Image_Container Column Align_Items_Center Justify_Content_Center Pan_Size_37 Pan_Shape_Round">
+							<div class="Shadow_Image_Container Column Align_Items_Center Justify_Content_Center Pan_Size_26 Pan_Shape_Square">
 								<div class="Wrapper">
-									<img src="../assets/temp-shadow-imgs/Mochi.jpg" />
-									<div class="Pan_Shadow">
-										<?php include "components/pan-controls.php"; ?>
-									</div>
-								</div>
-							</div>
-							<div class="Shadow_Image_Container Column Align_Items_Center Justify_Content_Center Pan_Size_37 Pan_Shape_Square">
-								<div class="Wrapper">
-									<img src="../assets/temp-shadow-imgs/ACS34.jpg" />
+									<img src="../assets/temp-shadow-imgs/ACM14.jpg" />
 									<div class="Pan_Shadow">
 										<?php include "components/pan-controls.php"; ?>
 									</div>
@@ -131,7 +123,15 @@
 							</div>
 							<div class="Shadow_Image_Container Column Align_Items_Center Justify_Content_Center Pan_Size_26 Pan_Shape_Round">
 								<div class="Wrapper">
-									<img src="../assets/temp-shadow-imgs/Bittermelon.jpg" />
+									<img src="../assets/temp-shadow-imgs/Buttercupcake.jpg" />
+									<div class="Pan_Shadow">
+										<?php include "components/pan-controls.php"; ?>
+									</div>
+								</div>
+							</div>
+							<div class="Shadow_Image_Container Column Align_Items_Center Justify_Content_Center Pan_Size_26 Pan_Shape_Square">
+								<div class="Wrapper">
+									<img src="../assets/temp-shadow-imgs/ACD00.jpg" />
 									<div class="Pan_Shadow">
 										<?php include "components/pan-controls.php"; ?>
 									</div>
@@ -145,6 +145,22 @@
 									</div>
 								</div>
 							</div>
+							<div class="Shadow_Image_Container Column Align_Items_Center Justify_Content_Center Pan_Size_37 Pan_Shape_Square">
+								<div class="Wrapper">
+									<img src="../assets/temp-shadow-imgs/ACD4.jpg" />
+									<div class="Pan_Shadow">
+										<?php include "components/pan-controls.php"; ?>
+									</div>
+								</div>
+							</div>
+							<div class="Shadow_Image_Container Column Align_Items_Center Justify_Content_Center Pan_Size_37 Pan_Shape_Round">
+								<div class="Wrapper">
+									<img src="../assets/temp-shadow-imgs/Gamma-Ray.jpg" />
+									<div class="Pan_Shadow">
+										<?php include "components/pan-controls.php"; ?>
+									</div>
+								</div>
+							</div>
 						</div>
 						<div class="Column Gap_16 Landscape">
 						<div class="Story_Container Story_Size_4w_3t Container">
@@ -152,6 +168,14 @@
 								<div class="Shadow_Image_Container Column Align_Items_Center Justify_Content_Center Pan_Size_37 Pan_Shape_Round">
 									<div class="Wrapper">
 										<img src="../assets/temp-shadow-imgs/Arsenic.jpg" />
+										<div class="Pan_Shadow">
+											<?php include "components/pan-controls.php"; ?>
+										</div>
+									</div>
+								</div>
+								<div class="Shadow_Image_Container Column Align_Items_Center Justify_Content_Center Pan_Size_26 Pan_Shape_Round">
+									<div class="Wrapper">
+										<img src="../assets/temp-shadow-imgs/Hot-Spiced-Cider.jpg" />
 										<div class="Pan_Shadow">
 											<?php include "components/pan-controls.php"; ?>
 										</div>
@@ -184,62 +208,6 @@
 								<div class="Shadow_Image_Container Column Align_Items_Center Justify_Content_Center Pan_Size_Irregular Pan_Shape_Irregular">
 									<div class="Wrapper">
 										<img src="../assets/temp-shadow-imgs/burnish.jpg" />
-										<div class="Pan_Shadow">
-											<?php include "components/pan-controls.php"; ?>
-										</div>
-									</div>
-								</div>
-								<div class="Shadow_Image_Container Column Align_Items_Center Justify_Content_Center Pan_Size_37 Pan_Shape_Round">
-									<div class="Wrapper">
-										<img src="../assets/temp-shadow-imgs/Gamma-Ray.jpg" />
-										<div class="Pan_Shadow">
-											<?php include "components/pan-controls.php"; ?>
-										</div>
-									</div>
-								</div>
-								<div class="Shadow_Image_Container Column Align_Items_Center Justify_Content_Center Pan_Size_26 Pan_Shape_Round">
-									<div class="Wrapper">
-										<img src="../assets/temp-shadow-imgs/Helix-Nebula.jpg" />
-										<div class="Pan_Shadow">
-											<?php include "components/pan-controls.php"; ?>
-										</div>
-									</div>
-								</div>
-								<div class="Shadow_Image_Container Column Align_Items_Center Justify_Content_Center Pan_Size_26 Pan_Shape_Round">
-									<div class="Wrapper">
-										<img src="../assets/temp-shadow-imgs/Hot-Spiced-Cider.jpg" />
-										<div class="Pan_Shadow">
-											<?php include "components/pan-controls.php"; ?>
-										</div>
-									</div>
-								</div>
-								<div class="Shadow_Image_Container Column Align_Items_Center Justify_Content_Center Pan_Size_26 Pan_Shape_Square">
-									<div class="Wrapper">
-										<img src="../assets/temp-shadow-imgs/ACM14.jpg" />
-										<div class="Pan_Shadow">
-											<?php include "components/pan-controls.php"; ?>
-										</div>
-									</div>
-								</div>
-								<div class="Shadow_Image_Container Column Align_Items_Center Justify_Content_Center Pan_Size_26 Pan_Shape_Round">
-									<div class="Wrapper">
-										<img src="../assets/temp-shadow-imgs/Buttercupcake.jpg" />
-										<div class="Pan_Shadow">
-											<?php include "components/pan-controls.php"; ?>
-										</div>
-									</div>
-								</div>
-								<div class="Shadow_Image_Container Column Align_Items_Center Justify_Content_Center Pan_Size_26 Pan_Shape_Square">
-									<div class="Wrapper">
-										<img src="../assets/temp-shadow-imgs/ACD00.jpg" />
-										<div class="Pan_Shadow">
-											<?php include "components/pan-controls.php"; ?>
-										</div>
-									</div>
-								</div>
-								<div class="Shadow_Image_Container Column Align_Items_Center Justify_Content_Center Pan_Size_37 Pan_Shape_Square">
-									<div class="Wrapper">
-										<img src="../assets/temp-shadow-imgs/ACD4.jpg" />
 										<div class="Pan_Shadow">
 											<?php include "components/pan-controls.php"; ?>
 										</div>
