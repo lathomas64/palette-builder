87af8f8e1d379e55734c09a61e70bae469b7d9e7
Imani Thomas <lathomas64@gmail.com>
2021-09-19 15:05:01 -0400

    removing unecessary return false

--- wp-content/plugins/builder/src/components/03_modals/user-sort.php
+++ wp-content/plugins/builder/src/components/03_modals/user-sort.php
@@ -20,35 +20,35 @@
      </div>
    <div class="Column Gap_0">
        <!-- data-name, data-publish-date data-size data-price Story_Card -->
-         <input onclick="sort_children('data-publish-date', '#User_Story_Target .Story_Size', false);return false;" class="Single_Select" type="radio" id="User_Publish_New_Old">
+         <input onclick="sort_children('data-publish-date', '#User_Story_Target .Story_Size', false);" class="Single_Select" type="radio" id="User_Publish_New_Old">
          <label class="Single_Select" for="User_Publish_New_Old">
          Publish Date: New to Old
          </label>
-         <input onclick="sort_children('data-publish-date', '#User_Story_Target .Story_Size', true);return false;" class="Single_Select" type="radio" id="User_Publish_Old_New">
+         <input onclick="sort_children('data-publish-date', '#User_Story_Target .Story_Size', true);" class="Single_Select" type="radio" id="User_Publish_Old_New">
          <label class="Single_Select" for="User_Publish_Old_New">
          Publish Date: Old to New
          </label>
-         <input onclick="sort_children('data-price', '#User_Story_Target .Story_Size', false);return false;" class="Single_Select" type="radio" id="User_Price_High_Low">
+         <input onclick="sort_children('data-price', '#User_Story_Target .Story_Size', false);" class="Single_Select" type="radio" id="User_Price_High_Low">
          <label class="Single_Select" for="User_Price_High_Low">
          Story Price: High to Low
          </label>
-         <input onclick="sort_children('data-price', '#User_Story_Target .Story_Size', true);return false;" class="Single_Select" type="radio" id="User_Price_Low_High">
+         <input onclick="sort_children('data-price', '#User_Story_Target .Story_Size', true);" class="Single_Select" type="radio" id="User_Price_Low_High">
          <label class="Single_Select" for="User_Price_Low_High">
          Story Price: Low to High
          </label>
-         <input onclick="sort_children('data-size', '#User_Story_Target .Story_Size', true);return false;" class="Single_Select" type="radio" id="User_Size_Small_Large">
+         <input onclick="sort_children('data-size', '#User_Story_Target .Story_Size', true);" class="Single_Select" type="radio" id="User_Size_Small_Large">
          <label class="Single_Select" for="User_Size_Small_Large">
          Story Size: Small to Large
          </label>
-         <input onclick="sort_children('data-size', '#User_Story_Target .Story_Size', false);return false;" class="Single_Select" type="radio" id="User_Size_Large_Small">
+         <input onclick="sort_children('data-size', '#User_Story_Target .Story_Size', false);" class="Single_Select" type="radio" id="User_Size_Large_Small">
          <label class="Single_Select" for="User_Size_Large_Small">
          Story Size: Large to Small
          </label>
-         <input onclick="sort_children('data-name', '#User_Story_Target .Story_Size', true);return false;" class="Single_Select" type="radio" id="User_Name_A_Z">
+         <input onclick="sort_children('data-name', '#User_Story_Target .Story_Size', true);" class="Single_Select" type="radio" id="User_Name_A_Z">
          <label class="Single_Select" for="User_Name_A_Z">
          Name: A to Z
          </label>
-         <input onclick="sort_children('data-name', '#User_Story_Target .Story_Size', false);return false;" class="Single_Select" type="radio" id="User_Name_Z_A">
+         <input onclick="sort_children('data-name', '#User_Story_Target .Story_Size', false);" class="Single_Select" type="radio" id="User_Name_Z_A">
          <label class="Single_Select" for="User_Name_Z_A">
          Name: Z to A
          </label>
