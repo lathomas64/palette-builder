3c4d82a877b444ee17af4029d2167c6c062aad3e
Imani Thomas <lathomas64@gmail.com>
2021-11-15 22:29:16 -0500

    fix double price and typo in shipping

--- wp-content/plugins/builder/src/components/01_filter-sections/price-shipping.php
+++ wp-content/plugins/builder/src/components/01_filter-sections/price-shipping.php
@@ -3,7 +3,6 @@
 		<?php include "components/01_filter-sections/price-range.php"; ?>
 	</div>
 		<div class="Filter_Section Column Gap_16">
-			<?php include "components/01_filter-sections/price-range.php"; ?>
-			<?php include "components/01_filter-sections/shipping-countries/php"; ?>
+			<?php include "components/01_filter-sections/shipping-countries.php"; ?>
 		</div>
 </div>
