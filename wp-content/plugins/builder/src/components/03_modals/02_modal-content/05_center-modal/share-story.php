17d894d2c3063cccbc5796d36d13fb3f3137b563
Gina <gina@ginaluttrell.com>
2021-11-17 14:24:53 +0000

    Starting share modal

---
+++ wp-content/plugins/builder/src/components/03_modals/02_modal-content/05_center-modal/share-story.php
@@ -0,0 +1,118 @@
+<div id="shareModal" class="Share_Modal"> <!-- Hide when finished-->
+	<div class="Wireframe Img_Container Column Space_Between">
+			<div class="Palette Story_Size_3w_3t Flex_Container">
+				<div id="Story_Grid" class="Story_Grid Column Justify_Content_Left Align_Items_Center">
+					<div class="Shadow_Image_Container Column Align_Items_Center Justify_Content_Center Pan_Shape_Round Pan_Size_26">
+  <!-- display none for the prototype version when this is used remove display none -->
+	<div class="Wrapper"><img src="https://pb.rainbowcapitalism.com/wp-content/uploads/2021/10/4thDimension-TerraMoonsCosmetics.jpg">
+		<!-- when we have an image add the image to the wrapper
+    <img src="../assets/temp-shadow-imgs/Arsenic.jpg" />
+    -->
+		<div class="Pan_Shadow">
+				</div>
+	</div>
+</div>
+					<div class="Shadow_Image_Container Column Align_Items_Center Justify_Content_Center Pan_Shape_Round Pan_Size_26">
+  <!-- display none for the prototype version when this is used remove display none -->
+	<div class="Wrapper"><img src="https://pb.rainbowcapitalism.com/wp-content/uploads/2021/10/4thDimension-TerraMoonsCosmetics.jpg">
+		<!-- when we have an image add the image to the wrapper
+    <img src="../assets/temp-shadow-imgs/Arsenic.jpg" />
+    -->
+		<div class="Pan_Shadow">
+				</div>
+	</div>
+</div>
+					<div class="Shadow_Image_Container Column Align_Items_Center Justify_Content_Center Pan_Shape_Round Pan_Size_26">
+  					<!-- display none for the prototype version when this is used remove display none -->
+						<div class="Wrapper"><img src="https://pb.rainbowcapitalism.com/wp-content/uploads/2021/10/4thDimension-TerraMoonsCosmetics.jpg">
+							<!-- when we have an image add the image to the wrapper
+    					<img src="../assets/temp-shadow-imgs/Arsenic.jpg" />
+    					-->
+							<div class="Pan_Shadow">
+									</div>
+						</div>
+					</div>
+					<div class="Shadow_Image_Container Column Align_Items_Center Justify_Content_Center Pan_Shape_Round Pan_Size_26">
+  					<!-- display none for the prototype version when this is used remove display none -->
+						<div class="Wrapper"><img src="https://pb.rainbowcapitalism.com/wp-content/uploads/2021/10/4thDimension-TerraMoonsCosmetics.jpg">
+							<!-- when we have an image add the image to the wrapper
+    					<img src="../assets/temp-shadow-imgs/Arsenic.jpg" />
+    					-->
+							<div class="Pan_Shadow">
+									</div>
+						</div>
+					</div>
+					<div class="Shadow_Image_Container Column Align_Items_Center Justify_Content_Center Pan_Shape_Round Pan_Size_26">
+  					<!-- display none for the prototype version when this is used remove display none -->
+						<div class="Wrapper"><img src="https://pb.rainbowcapitalism.com/wp-content/uploads/2021/10/4thDimension-TerraMoonsCosmetics.jpg">
+							<!-- when we have an image add the image to the wrapper
+    					<img src="../assets/temp-shadow-imgs/Arsenic.jpg" />
+    					-->
+							<div class="Pan_Shadow">
+									</div>
+						</div>
+					</div>
+					<div class="Shadow_Image_Container Column Align_Items_Center Justify_Content_Center Pan_Shape_Round Pan_Size_26">
+  					<!-- display none for the prototype version when this is used remove display none -->
+						<div class="Wrapper"><img src="https://pb.rainbowcapitalism.com/wp-content/uploads/2021/10/4thDimension-TerraMoonsCosmetics.jpg">
+							<!-- when we have an image add the image to the wrapper
+    					<img src="../assets/temp-shadow-imgs/Arsenic.jpg" />
+    					-->
+							<div class="Pan_Shadow">
+									</div>
+						</div>
+					</div>
+					<div class="Shadow_Image_Container Column Align_Items_Center Justify_Content_Center Pan_Shape_Round Pan_Size_26">
+  <!-- display none for the prototype version when this is used remove display none -->
+	<div class="Wrapper"><img src="https://pb.rainbowcapitalism.com/wp-content/uploads/2021/10/4thDimension-TerraMoonsCosmetics.jpg">
+		<!-- when we have an image add the image to the wrapper
+    <img src="../assets/temp-shadow-imgs/Arsenic.jpg" />
+    -->
+		<div class="Pan_Shadow">
+				</div>
+	</div>
+</div>
+					<div class="Shadow_Image_Container Column Align_Items_Center Justify_Content_Center Pan_Shape_Round Pan_Size_26">
+  <!-- display none for the prototype version when this is used remove display none -->
+	<div class="Wrapper"><img src="https://pb.rainbowcapitalism.com/wp-content/uploads/2021/10/4thDimension-TerraMoonsCosmetics.jpg">
+		<!-- when we have an image add the image to the wrapper
+    <img src="../assets/temp-shadow-imgs/Arsenic.jpg" />
+    -->
+		<div class="Pan_Shadow">
+				</div>
+	</div>
+</div>
+<div class="Shadow_Image_Container Column Align_Items_Center Justify_Content_Center Pan_Shape_Round Pan_Size_26">
+  <!-- display none for the prototype version when this is used remove display none -->
+	<div class="Wrapper"><img src="https://pb.rainbowcapitalism.com/wp-content/uploads/2021/10/4thDimension-TerraMoonsCosmetics.jpg">
+		<!-- when we have an image add the image to the wrapper
+    <img src="../assets/temp-shadow-imgs/Arsenic.jpg" />
+    -->
+		<div class="Pan_Shadow">
+				</div>
+	</div>
+</div>
+				</div>
+			</div>
+			<div class="Wireframe Row Space_Between">
+				<div class="Row Gap_16">Watermark</div>
+				<div class="Row Gap_16">Short Link</div>
+			</div>
+<!-- <div class="Wireframe Row Space_Between">
+				<div class="Row Gap_16">BG controls</div>
+				<div class="Row Gap_16">Shadow name controls</div>
+			</div> -->			
+	</div>
+		<div class="Wireframe Row Gap_16">
+			<div>Ready?</div>
+			<div class="Wireframe Row Gap_8">
+			<div class="Wireframe Share_Icon">I</div>
+			<div class="Wireframe Share_Icon">F</div>
+			<div class="Wireframe Share_Icon">T</div>
+			<div class="Wireframe Share_Icon">R</div>
+			<div class="Wireframe Share_Icon">L</div>
+			<div class="Wireframe Share_Icon">D</div>
+			</div>
+		</div>
+	</div>
+</div>
