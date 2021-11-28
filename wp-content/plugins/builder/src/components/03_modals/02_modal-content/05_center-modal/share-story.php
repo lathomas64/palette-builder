5db98628114e442105ef1fbb528e9c50f5edeea4
Gina <gina@ginaluttrell.com>
2021-11-28 18:51:38 +0000

    Piddling on share modal

--- wp-content/plugins/builder/src/components/03_modals/02_modal-content/05_center-modal/share-story.php
+++ wp-content/plugins/builder/src/components/03_modals/02_modal-content/05_center-modal/share-story.php
@@ -1,5 +1,6 @@
 <div id="shareModal" class="Share_Modal"> <!-- Hide when finished-->
 	<div class="Wireframe Img_Container Column Space_Between">
+		<div class="Column Palette_Container">
 			<div class="Palette Story_Size_3w_3t Flex_Container">
 				<div id="Story_Grid" class="Story_Grid Column Justify_Content_Left Align_Items_Center">
 					<div class="Shadow_Image_Container Column Align_Items_Center Justify_Content_Center Pan_Shape_Round Pan_Size_26">
@@ -93,16 +94,20 @@
 	</div>
 </div>
 				</div>
-			</div>
-			<div class="Wireframe Row Space_Between">
-				<div class="Row Gap_16">Watermark</div>
-				<div class="Row Gap_16">Short Link</div>
-			</div>
+			</div> 
+		</div>
+		<div class="Img_Meta Row Space_Between">
+			<div class="Row Gap_16">Watermark</div>
+			<div class="Row Gap_16">Short Link</div>
+		</div>
+<!-- ^^36 padding here -->
 <!-- <div class="Wireframe Row Space_Between">
 				<div class="Row Gap_16">BG controls</div>
 				<div class="Row Gap_16">Shadow name controls</div>
 			</div> -->			
 	</div>
+
+<!-- ^^16 padding here -->
 		<div class="Wireframe Row Gap_16">
 			<div>Ready?</div>
 			<div class="Wireframe Row Gap_8">
