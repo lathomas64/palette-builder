dc56be66d23edc3c9cabd0d135eddcdd5b1bc8f9
Gina <gina@ginaluttrell.com>
2021-12-05 19:56:20 +0000

    More modal share stuff

--- wp-content/plugins/builder/src/components/03_modals/01_modal-formats/05_generic-modal.php
+++ wp-content/plugins/builder/src/components/03_modals/01_modal-formats/05_generic-modal.php
@@ -1,6 +1,3 @@
 <div id="centerModal" class="Modal Column Fade_In On">
-	<div class="Row Reverse Space_Between Drawer_Overlay">
-    <div class="Drawer_Container Fade_Out_Right">	
-      <?php include "components/03_modals/02_modal-content/05_center-modal/share-story.php"; ?></div>
-  </div>
+      <?php include "components/03_modals/02_modal-content/05_center-modal/share-story.php"; ?>
 </div>
--- wp-content/plugins/builder/src/components/03_modals/02_modal-content/05_center-modal/share-story.php
+++ wp-content/plugins/builder/src/components/03_modals/02_modal-content/05_center-modal/share-story.php
@@ -1,122 +1,156 @@
-<div id="shareModal" class="Share_Modal"> <!-- Hide when finished-->
-	<div class="Img_Container Dark Column Space_Between">
-		<div class="Row Palette_Container ">
-			<div class="Palette Story_Size_4w_3t Row">
-				<div id="Story_Grid" class="Story_Grid Column Justify_Content_Left Align_Items_Center">
-					<div class="Shadow_Image_Container Column Align_Items_Center Justify_Content_Center Pan_Shape_Round Pan_Size_26">
-  <!-- display none for the prototype version when this is used remove display none -->
-	<div class="Wrapper"><img src="https://pb.rainbowcapitalism.com/wp-content/uploads/2021/10/4thDimension-TerraMoonsCosmetics.jpg">
-		<!-- when we have an image add the image to the wrapper
-    <img src="../assets/temp-shadow-imgs/Arsenic.jpg" />
-    -->
-		<div class="Pan_Shadow">
-				</div>
-	</div>
-</div>
-					<div class="Shadow_Image_Container Column Align_Items_Center Justify_Content_Center Pan_Shape_Round Pan_Size_26">
-  <!-- display none for the prototype version when this is used remove display none -->
-	<div class="Wrapper"><img src="https://pb.rainbowcapitalism.com/wp-content/uploads/2021/10/4thDimension-TerraMoonsCosmetics.jpg">
-		<!-- when we have an image add the image to the wrapper
-    <img src="../assets/temp-shadow-imgs/Arsenic.jpg" />
-    -->
-		<div class="Pan_Shadow">
-				</div>
-	</div>
-</div>
-					<div class="Shadow_Image_Container Column Align_Items_Center Justify_Content_Center Pan_Shape_Round Pan_Size_26">
-  					<!-- display none for the prototype version when this is used remove display none -->
-						<div class="Wrapper"><img src="https://pb.rainbowcapitalism.com/wp-content/uploads/2021/10/4thDimension-TerraMoonsCosmetics.jpg">
-							<!-- when we have an image add the image to the wrapper
-    					<img src="../assets/temp-shadow-imgs/Arsenic.jpg" />
-    					-->
-							<div class="Pan_Shadow">
-									</div>
+<div id="shareModal" class="Share_Modal Hidden"> <!-- Hide when finished-->
+	<div class="Grid_Container Modal_Grid Space_Between Drawer_Overlay"> 
+		<div class="Column Gap_24">
+			<div class="Panel_Title">Share Your Palette</div>
+			<div class="Subheading">Tweak any details you want with the buttons below, then click to share your palette!</div>
+			<div class="Column Share_Img_Format">
+				<form class="Column" action="/action_page.php">
+ 					<div class="Form_Group Row Gap_8 Align_Items_Flex_End">
+							<span>Background:</span>
+							<div class="Row Gap_8 Align_Items_Center">
+								<div class="Row Gap_4 Align_Items_flex_End">
+									<input type="radio" id="html" name="fav_language" value="HTML">
+									<label for="html">Light</label>
+								</div>
+								<div class="Row Gap_4 Align_Items_flex_End">
+									<input type="radio" id="html" name="fav_language" value="HTML">
+									<label for="html">Dark</label>
+								</div>
+							</div>
 						</div>
-					</div>
-					<div class="Shadow_Image_Container Column Align_Items_Center Justify_Content_Center Pan_Shape_Round Pan_Size_26">
-  					<!-- display none for the prototype version when this is used remove display none -->
-						<div class="Wrapper"><img src="https://pb.rainbowcapitalism.com/wp-content/uploads/2021/10/4thDimension-TerraMoonsCosmetics.jpg">
-							<!-- when we have an image add the image to the wrapper
-    					<img src="../assets/temp-shadow-imgs/Arsenic.jpg" />
-    					-->
-							<div class="Pan_Shadow">
-									</div>
+ 					<div class="Form_Group Row Gap_8 Align_Items_Flex_End">
+							<span>Shadow Names:</span>
+							<div class="Row Gap_8 Align_Items_Center">
+								<div class="Row Gap_4 Align_Items_flex_End">
+									<input type="radio" id="html" name="fav_language" value="HTML">
+									<label for="html">Show</label>
+								</div>
+								<div class="Row Gap_4 Align_Items_flex_End">
+									<input type="radio" id="html" name="fav_language" value="HTML">
+									<label for="html">Don't Show</label>
+								</div>
+							</div>
 						</div>
-					</div>
-					<div class="Shadow_Image_Container Column Align_Items_Center Justify_Content_Center Pan_Shape_Round Pan_Size_26">
-  					<!-- display none for the prototype version when this is used remove display none -->
-						<div class="Wrapper"><img src="https://pb.rainbowcapitalism.com/wp-content/uploads/2021/10/4thDimension-TerraMoonsCosmetics.jpg">
-							<!-- when we have an image add the image to the wrapper
-    					<img src="../assets/temp-shadow-imgs/Arsenic.jpg" />
-    					-->
-							<div class="Pan_Shadow">
-									</div>
+				</form>
+</div>	
+			<div class="Wireframe Row Gap_16">
+				<div>Ready?</div>
+				<div class="Wireframe Row Gap_8">
+				<div class="Wireframe Share_Icon">I</div>
+				<div class="Wireframe Share_Icon">F</div>
+				<div class="Wireframe Share_Icon">T</div>
+				<div class="Wireframe Share_Icon">R</div>
+				<div class="Wireframe Share_Icon">L</div>
+				<div class="Wireframe Share_Icon">D</div>
+				</div>
+			</div>
+			<button class="Text_Button Row Gap_4 Align_Items_Center"><div class="Icon_Container Column Justify_Content_Center">
+					  <svg width="9" height="14" viewBox="0 0 9 14" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M8.645 12.3433L3.30167 7L8.645 1.645L7 0L0 7L7 14L8.645 12.3433Z"></path></svg>
+						</div>Go back to your palette</button>
+			</div>
+		<div class="Column">
+  	<div class="Img_Container Dark Column Space_Between">
+				<div class="Row Palette_Container">
+					<div class="Palette Story_Size_4w_3t Row">
+						<div id="Story_Grid" class="Story_Grid Column Justify_Content_Left Align_Items_Center">
+							<div class="Shadow_Image_Container Column Align_Items_Center Justify_Content_Center Pan_Shape_Round Pan_Size_26">
+ 			<!-- display none for the prototype version when this is used remove display none -->
+			<div class="Wrapper"><img src="https://pb.rainbowcapitalism.com/wp-content/uploads/2021/10/4thDimension-TerraMoonsCosmetics.jpg">
+				<!-- when we have an image add the image to the wrapper
+   			<img src="../assets/temp-shadow-imgs/Arsenic.jpg" />
+   			-->
+				<div class="Pan_Shadow">
 						</div>
-					</div>
-					<div class="Shadow_Image_Container Column Align_Items_Center Justify_Content_Center Pan_Shape_Round Pan_Size_26">
-  					<!-- display none for the prototype version when this is used remove display none -->
-						<div class="Wrapper"><img src="https://pb.rainbowcapitalism.com/wp-content/uploads/2021/10/4thDimension-TerraMoonsCosmetics.jpg">
-							<!-- when we have an image add the image to the wrapper
-    					<img src="../assets/temp-shadow-imgs/Arsenic.jpg" />
-    					-->
-							<div class="Pan_Shadow">
-									</div>
+			</div>
+			</div>
+							<div class="Shadow_Image_Container Column Align_Items_Center Justify_Content_Center Pan_Shape_Round Pan_Size_26">
+ 			<!-- display none for the prototype version when this is used remove display none -->
+			<div class="Wrapper"><img src="https://pb.rainbowcapitalism.com/wp-content/uploads/2021/10/4thDimension-TerraMoonsCosmetics.jpg">
+				<!-- when we have an image add the image to the wrapper
+   			<img src="../assets/temp-shadow-imgs/Arsenic.jpg" />
+   			-->
+				<div class="Pan_Shadow">
 						</div>
-					</div>
-					<div class="Shadow_Image_Container Column Align_Items_Center Justify_Content_Center Pan_Shape_Round Pan_Size_26">
-  <!-- display none for the prototype version when this is used remove display none -->
-	<div class="Wrapper"><img src="https://pb.rainbowcapitalism.com/wp-content/uploads/2021/10/4thDimension-TerraMoonsCosmetics.jpg">
-		<!-- when we have an image add the image to the wrapper
-    <img src="../assets/temp-shadow-imgs/Arsenic.jpg" />
-    -->
-		<div class="Pan_Shadow">
+			</div>
+			</div>
+							<div class="Shadow_Image_Container Column Align_Items_Center Justify_Content_Center Pan_Shape_Round Pan_Size_26">
+ 								<!-- display none for the prototype version when this is used remove display none -->
+								<div class="Wrapper"><img src="https://pb.rainbowcapitalism.com/wp-content/uploads/2021/10/4thDimension-TerraMoonsCosmetics.jpg">
+									<!-- when we have an image add the image to the wrapper
+   								<img src="../assets/temp-shadow-imgs/Arsenic.jpg" />
+   								-->
+									<div class="Pan_Shadow">
+											</div>
+								</div>
+							</div>
+							<div class="Shadow_Image_Container Column Align_Items_Center Justify_Content_Center Pan_Shape_Round Pan_Size_26">
+ 								<!-- display none for the prototype version when this is used remove display none -->
+								<div class="Wrapper"><img src="https://pb.rainbowcapitalism.com/wp-content/uploads/2021/10/4thDimension-TerraMoonsCosmetics.jpg">
+									<!-- when we have an image add the image to the wrapper
+   								<img src="../assets/temp-shadow-imgs/Arsenic.jpg" />
+   								-->
+									<div class="Pan_Shadow">
+											</div>
+								</div>
+							</div>
+							<div class="Shadow_Image_Container Column Align_Items_Center Justify_Content_Center Pan_Shape_Round Pan_Size_26">
+ 								<!-- display none for the prototype version when this is used remove display none -->
+								<div class="Wrapper"><img src="https://pb.rainbowcapitalism.com/wp-content/uploads/2021/10/4thDimension-TerraMoonsCosmetics.jpg">
+									<!-- when we have an image add the image to the wrapper
+   								<img src="../assets/temp-shadow-imgs/Arsenic.jpg" />
+   								-->
+									<div class="Pan_Shadow">
+											</div>
+								</div>
+							</div>
+							<div class="Shadow_Image_Container Column Align_Items_Center Justify_Content_Center Pan_Shape_Round Pan_Size_26">
+ 								<!-- display none for the prototype version when this is used remove display none -->
+								<div class="Wrapper"><img src="https://pb.rainbowcapitalism.com/wp-content/uploads/2021/10/4thDimension-TerraMoonsCosmetics.jpg">
+									<!-- when we have an image add the image to the wrapper
+   								<img src="../assets/temp-shadow-imgs/Arsenic.jpg" />
+   								-->
+									<div class="Pan_Shadow">
+											</div>
+								</div>
+							</div>
+							<div class="Shadow_Image_Container Column Align_Items_Center Justify_Content_Center Pan_Shape_Round Pan_Size_26">
+ 			<!-- display none for the prototype version when this is used remove display none -->
+			<div class="Wrapper"><img src="https://pb.rainbowcapitalism.com/wp-content/uploads/2021/10/4thDimension-TerraMoonsCosmetics.jpg">
+				<!-- when we have an image add the image to the wrapper
+   			<img src="../assets/temp-shadow-imgs/Arsenic.jpg" />
+   			-->
+				<div class="Pan_Shadow">
+						</div>
+			</div>
+			</div>
+							<div class="Shadow_Image_Container Column Align_Items_Center Justify_Content_Center Pan_Shape_Round Pan_Size_26">
+ 			<!-- display none for the prototype version when this is used remove display none -->
+			<div class="Wrapper"><img src="https://pb.rainbowcapitalism.com/wp-content/uploads/2021/10/4thDimension-TerraMoonsCosmetics.jpg">
+				<!-- when we have an image add the image to the wrapper
+   			<img src="../assets/temp-shadow-imgs/Arsenic.jpg" />
+   			-->
+				<div class="Pan_Shadow">
+						</div>
+			</div>
+			</div>
+							<div class="Shadow_Image_Container Column Align_Items_Center Justify_Content_Center Pan_Shape_Round Pan_Size_26">
+ 			<!-- display none for the prototype version when this is used remove display none -->
+			<div class="Wrapper"><img src="https://pb.rainbowcapitalism.com/wp-content/uploads/2021/10/4thDimension-TerraMoonsCosmetics.jpg">
+				<!-- when we have an image add the image to the wrapper
+   			<img src="../assets/temp-shadow-imgs/Arsenic.jpg" />
+   			-->
+				<div class="Pan_Shadow">
+						</div>
+			</div>
+			</div>
+						</div>
+					</div> 
 				</div>
-	</div>
-</div>
-					<div class="Shadow_Image_Container Column Align_Items_Center Justify_Content_Center Pan_Shape_Round Pan_Size_26">
-  <!-- display none for the prototype version when this is used remove display none -->
-	<div class="Wrapper"><img src="https://pb.rainbowcapitalism.com/wp-content/uploads/2021/10/4thDimension-TerraMoonsCosmetics.jpg">
-		<!-- when we have an image add the image to the wrapper
-    <img src="../assets/temp-shadow-imgs/Arsenic.jpg" />
-    -->
-		<div class="Pan_Shadow">
-				</div>
-	</div>
-</div>
-<div class="Shadow_Image_Container Column Align_Items_Center Justify_Content_Center Pan_Shape_Round Pan_Size_26">
-  <!-- display none for the prototype version when this is used remove display none -->
-	<div class="Wrapper"><img src="https://pb.rainbowcapitalism.com/wp-content/uploads/2021/10/4thDimension-TerraMoonsCosmetics.jpg">
-		<!-- when we have an image add the image to the wrapper
-    <img src="../assets/temp-shadow-imgs/Arsenic.jpg" />
-    -->
-		<div class="Pan_Shadow">
-				</div>
-	</div>
-</div>
-				</div>
-			</div> 
+				<div class="Img_Meta Row Space_Between  Align_Items_Flex_End">
+					<div class="Watermark Row Gap_16">&nbsp;</div>
+					<div class="Link Row Gap_16">bit.ly/2KXjkLX</div>
 		</div>
-		<div class="Img_Meta Row Space_Between  Align_Items_Flex_End">
-			<div class="Watermark Row Gap_16">&nbsp;</div>
-			<div class="Link Row Gap_16">bit.ly/2KXjkLX</div>
-		</div>
-<!-- <div class="Wireframe Row Space_Between">
-				<div class="Row Gap_16">BG controls</div>
-				<div class="Row Gap_16">Shadow name controls</div>
-			</div> -->			
-	</div>
-
-<!-- ^^16 padding here -->
-		<div class="Wireframe Row Gap_16">
-			<div>Ready?</div>
-			<div class="Wireframe Row Gap_8">
-			<div class="Wireframe Share_Icon">I</div>
-			<div class="Wireframe Share_Icon">F</div>
-			<div class="Wireframe Share_Icon">T</div>
-			<div class="Wireframe Share_Icon">R</div>
-			<div class="Wireframe Share_Icon">L</div>
-			<div class="Wireframe Share_Icon">D</div>
 			</div>
 		</div>
-	</div>
+ </div>
 </div>
