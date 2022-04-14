aee35157808d923ef51d7b96e83dec1688065738
Gina <gina@ginaluttrell.com>
2022-04-14 15:45:59 +0000

    Tweaks to shopping list tiles

--- wp-content/plugins/builder/src/components/shopping-list-shadow-card.php
+++ wp-content/plugins/builder/src/components/shopping-list-shadow-card.php
@@ -1,20 +1,19 @@
 <div class="List_Tile Grid_Container">
-<div class="Shadow_Image_Container Column Align_Items_Center Justify_Content_Center Pan_Size_26 Pan_Shape_Round">
+	<div class="Shadow_Image Column Align_Items_Center Justify_Content_Center">
+		<div class="Shadow_Image_Container Column Align_Items_Center Justify_Content_Center Pan_Size_26 Pan_Shape_Round">
 	<div class="Wrapper">
 		<img src="../assets/temp-shadow-imgs/Arctic-Lime.jpg">
 		<div class="Pan_Shadow"></div>
 	</div>
 </div>
-	<div class="Shade_Name">On a Whimsy</div>
-	<div class="Shadow Badge Shade_Size_Shape">Round, 26mm</div>
-	<div class="Shadow_Info Align_Items_Baseline Row Gap_8">
-		<div class="Shadow Badge Shade_Finish">Metallic</div><div class="vr">&nbsp;</div>
+	</div>
+	<div class="Shade_Name Column Justify_Content_Flex_End">On a Whimsy</div>
+	<div class="Shade_Size_Shape">Round, 26mm</div>
+	<div class="Shadow_Info Align_Items_Center Row Gap_0">
+		<div class="Shadow Badge Shade_Finish">Metallic</div>
 		<div class="Shadow Badge Shade_Shift">Duochrome</div>
-		<div class="vr">&nbsp;</div>
 		<div class="Shadow Badge Shade_Color">Warm Neutral </div>
-		<div class="vr">&nbsp;</div>
 		<div class="Shadow Badge Shade_Vividness">Muted</div>
-		<div class="vr">&nbsp;</div>
 		<div class="Shadow Badge Shade_Lightness">Light</div>
 	</div>
 	<div class="Price_Widget Reverse Column Gap_2 Justify_Content_Center" style="text-align: right;">
