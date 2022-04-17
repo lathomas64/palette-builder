34bc955a19907b3809c5bc37aa292bd724c57adb
Gina <gina@ginaluttrell.com>
2022-04-17 23:17:13 +0000

    Pan Controls update

--- wp-content/plugins/builder/src/components/pan-controls.php
+++ wp-content/plugins/builder/src/components/pan-controls.php
@@ -1,27 +1,25 @@
-<div class="Shadow_Controls Column Space_Between">
+<div class="Shadow_Controls Grid_Container">
 	<div class="Arrow Up_Arrow Row Justify_Content_Center">
 		<div class="Icon_Container"><?php include "../assets/01_Icons/pan-controls/arrow-up.svg"; ?></div>
 	</div>
-	<div class="Row Space_Between Align_Items_Center">
-		<div class="Column Arrow Left_Arrow">
-			<div class="Icon_Container"><?php include "../assets/01_Icons/pan-controls/arrow-left.svg"; ?>
-			</div>
-		</div>
-		<div class="Column Shadow_Name">Reign of Terra</div>
-		<div class="Column Arrow Right_Arrow">
-			<div class="Icon_Container"><?php include "../assets/01_Icons/pan-controls/arrow-right.svg"; ?>
-			</div>
+	<div class="Arrow Left_Arrow Justify_Content_Center Column">
+		<div class="Icon_Container"><?php include "../assets/01_Icons/pan-controls/arrow-left.svg"; ?>
 		</div>
 	</div>
-	<div class="Row Space_Between Align_Items_Center">
-		<div class="Column Arrow Place_Aside">
-			<div class="Icon_Container"><?php include "../assets/01_Icons/pan-controls/arrow-left.svg"; ?>
+	<div class="Shadow_Name_Container Column Justify_Content_Center Align_Items_Center">
+		<div class="Shadow_Name">Reign of Terra</div>
+</div>
+	<div class="Arrow Right_Arrow Justify_Content_Center Column">
+		<div class="Icon_Container"><?php include "../assets/01_Icons/pan-controls/arrow-right.svg"; ?>
 		</div>
-		</div>
-		<div class="Column Arrow Down_Arrow"><div class="Icon_Container"><?php include "../assets/01_Icons/pan-controls/arrow-down.svg"; ?>
-		</div></div>
-		<div class="Column Arrow Remove">
+	</div>
+	<div class="Arrow Place_Aside Justify_Content_Center Column" style="display:none;">
+		<div class="Icon_Container"><?php include "../assets/01_Icons/pan-controls/arrow-left.svg"; ?>
+	</div>
+	</div>
+	<div class="Arrow Down_Arrow Justify_Content_Center Row"><div class="Icon_Container"><?php include "../assets/01_Icons/pan-controls/arrow-down.svg"; ?>
+	</div></div>
+		<div class="Arrow Remove Justify_Content_Center Column">
 			<div class="Icon_Container"><?php include "../assets/01_Icons/pan-controls/trash.svg"; ?></div>
 		</div>
-	</div>
 </div>
