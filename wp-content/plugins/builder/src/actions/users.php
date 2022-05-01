fdb1874405b1a4431b2e44b2386dc612f7c4ef5b
Imani Thomas <lathomas64@gmail.com>
2022-05-01 16:08:24 -0400

    comment notes on what to look for next time

--- wp-content/plugins/builder/src/actions/users.php
+++ wp-content/plugins/builder/src/actions/users.php
@@ -1,5 +1,12 @@
 <?php
 function gumroad_register() {
+  //pull data from request
+  //maybe it will be ping data: https://app.gumroad.com/ping plus password
+  //verify data against gumroad
+  //https://api.gumroad.com/v2/sales/:id
+  //if data is good make user account at subscriber level
+  //wp_create_user( 'johndoe', 'passwordgoeshere', 'john.doe@example.com' );
+  //https://developer.wordpress.org/reference/functions/wp_create_user/
   return 'ack';
 }
 
