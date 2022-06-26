be0ec0e3eb90447861969657a20fdbf1b3c530ea
Imani Thomas <lathomas64@gmail.com>
2022-06-26 11:25:52 -0400

    register_gumroad action set up to check license

--- wp-content/plugins/builder/src/actions/users.php
+++ wp-content/plugins/builder/src/actions/users.php
@@ -7,7 +7,27 @@ function gumroad_register() {
   //if data is good make user account at subscriber level
   //wp_create_user( 'johndoe', 'passwordgoeshere', 'john.doe@example.com' );
   //https://developer.wordpress.org/reference/functions/wp_create_user/
-  echo(json_encode($_REQUEST));
+
+  # PING -> license key -> verify license -> if success create user
+
+  # IDEA: use https://api.gumroad.com/v2/licenses/verify to verify
+  $ch = curl_init ( 'https://api.gumroad.com/v2/licenses/verify' );
+  curl_setopt_array ( $ch, array (
+          CURLOPT_POST => 1,
+          CURLOPT_RETURNTRANSFER => true,
+          CURLOPT_POSTFIELDS => array (
+                  'license_key' => $_REQUEST["license_key"],
+                  'product_permalink' => "khiHk"
+          )
+  ) );
+  $response = json_decode(curl_exec ( $ch ), true);
+  curl_close($ch);
+  if($response["success"]) {
+    wp_create_user( $_REQUEST["username"], $_REQUEST["password"], $_REQUEST["email"] );
+    echo("user created");
+  } else {
+    echo($response["message"]);
+  }
   die();
 }
 
